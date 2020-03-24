2ff4927edaa00104ee3863f0400c66e48a9797cd
sanjana <sanjanabalamurugan1999@gmail.com>
2020-03-24 17:14:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/email/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.email;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.email.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eTo;
+    private EditText eSubject;
+    private EditText eMsg;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eTo = (EditText)findViewById(R.id.txtTo);
+        eSubject = (EditText)findViewById(R.id.txtSub);
+        eMsg = (EditText)findViewById(R.id.txtMsg);
+        btn = (Button)findViewById(R.id.btnSend);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Intent.ACTION_SEND);
+                it.putExtra(Intent.EXTRA_EMAIL, new String[]{eTo.getText().toString()});
+                it.putExtra(Intent.EXTRA_SUBJECT,eSubject.getText().toString());
+                it.putExtra(Intent.EXTRA_TEXT,eMsg.getText());
+                it.setType("message/rfc822");
+                startActivity(Intent.createChooser(it,"Choose Mail App"));
+            }
+        });
+    }
+}
